69a854e97e5a3e046186d65be2d026d6f9ff73a4
18372738 <133884450+18372738@users.noreply.github.com>
2024-08-23 12:46:05 +0300

    Adding images to db via place

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Picture, Place
 
 
+class ModelInline(admin.TabularInline):
+    model = Picture
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ModelInline,
+    ]
+
+
 admin.site.register(Picture)
-admin.site.register(Place)
